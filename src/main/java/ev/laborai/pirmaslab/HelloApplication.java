1046de33f41251d51c47a4186c545c14f32429be
Evaldas Visockas <vievaldas@gmail.com>
2022-02-25 11:25:42 +0200

    Setup

---
+++ src/main/java/ev/laborai/pirmaslab/HelloApplication.java
@@ -0,0 +1,9 @@
+package ev.laborai.pirmaslab;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
