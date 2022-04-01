5546e589f6b3f7b9fb0d234a887bdb8449ab8359
Evaldas Visockas <vievaldas@gmail.com>
2022-04-01 18:08:50 +0300

    Simplify and relocate component

--- src/main/java/ev/laborai/pirmaslab/services/NicknameSuggestingComponent.java
+++ src/main/java/ev/laborai/pirmaslab/services/NicknameSuggestingComponent.java
@@ -1,27 +1,27 @@
-package ev.laborai.pirmaslab.usecases;
+package ev.laborai.pirmaslab.services;
 
-import ev.laborai.pirmaslab.services.NicknameSuggester;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @SessionScoped
 @Named
 public class NicknameSuggestingComponent implements Serializable {
-    @Inject
-    private NicknameSuggester nicknameSuggester;
+
+    private static final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345674890";
+    private static final int size = 10;
 
     private CompletableFuture<String> suggesterTask;
 
     public String suggestNickname(String currentNickname) throws ExecutionException, InterruptedException {
         Map<String, String> requestParameters =
                 FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-        suggesterTask = CompletableFuture.supplyAsync(() -> nicknameSuggester.suggestNickname(currentNickname));
+        suggesterTask = CompletableFuture.supplyAsync(() -> suggestNick(currentNickname));
         return "/rider?faces-redirect=true&riderId=" + requestParameters.get("riderId");
     }
 
@@ -36,6 +36,20 @@ public class NicknameSuggestingComponent implements Serializable {
             return "Nickname is being generated.";
         }
         return "Suggested nickname: " + suggesterTask.get();
+    }
+
+    public String suggestNick(String currentNickname) {
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+        StringBuilder generatedNickname = new StringBuilder(currentNickname);
+        Random random = new Random();
+        for(int i = 0; i < size; i++) {
+            int chosenIndex = random.nextInt(lexicon.length());
+            char chosenLetter = lexicon.charAt(chosenIndex);
+            generatedNickname.append(chosenLetter);
+        }
+        return generatedNickname.toString();
     }
 
 }
